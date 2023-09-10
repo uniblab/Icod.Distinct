354c128bc2413a3a732782482fab541c64ebc066
uniblab <uniblab@hotmail.com>
2023-09-09 22:01:21 -0400

    chore: fix copyright

--- src/Program.cs
+++ src/Program.cs
@@ -102,7 +102,6 @@ namespace Icod.Distinct {
 		private static void PrintCopyright() {
 			var copy = new System.String[] {
 				"Distinct.exe returns only unique lines of text from input.",
-				"",
 				"Copyright( C ) 2023 Timothy J. Bruce",
 				"",
 				"This program is free software: you can redistribute it and / or modify",
@@ -110,17 +109,12 @@ namespace Icod.Distinct {
 				"the Free Software Foundation, either version 3 of the License, or",
 				"( at your option ) any later version.",
 				"",
-				"",
 				"This program is distributed in the hope that it will be useful,",
 				"but WITHOUT ANY WARRANTY; without even the implied warranty of",
-				"",
-				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the",
-				"",
+				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
 				"GNU General Public License for more details.",
 				"",
-				"",
 				"You should have received a copy of the GNU General Public License",
-				"",
 				"along with this program.If not, see < https://www.gnu.org/licenses/>."
 			};
 			foreach ( var line in copy ) {
