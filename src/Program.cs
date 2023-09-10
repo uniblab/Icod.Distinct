90320ea2410eb4d639bb2d7d6671fdfb0a13b93e
uniblab <uniblab@hotmail.com>
2023-09-10 17:56:57 -0400

    upgraded argh reference updated arg logic

--- src/Program.cs
+++ src/Program.cs
@@ -24,60 +24,71 @@ namespace Icod.Distinct {
 
 		[System.STAThread]
 		public static System.Int32 Main( System.String[] args ) {
-			if ( null == args ) {
-				args = new System.String[ 0 ];
-			}
 			var len = args.Length;
 			if ( 8 < len ) {
 				PrintUsage();
 				return 1;
 			}
-			System.String? inputPathName = null;
-			System.String? outputPathName = null;
-			System.String name = "EN-US";
-			var ignoreCase = true;
-			if ( 0 < len ) {
-				--len;
-				System.String @switch;
-				System.Int32 i = -1;
-				do {
-					@switch = args[ ++i ];
-					if ( new System.String[] { "--help", "-h", "/h" }.Contains( @switch, System.StringComparer.OrdinalIgnoreCase ) ) {
-						PrintUsage();
-						return 1;
-					} else if ( new System.String[] { "--copyright", "-c", "/c" }.Contains( @switch, System.StringComparer.OrdinalIgnoreCase ) ) {
-						PrintCopyright();
-						return 1;
-					} else if ( "--input".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-						inputPathName = args[ ++i ].TrimToNull();
-					} else if ( "--output".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-						outputPathName = args[ ++i ].TrimToNull();
-					} else if ( "--name".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-						name = args[ ++i ].TrimToNull()!;
-					} else if ( "--ignoreCase".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-						if ( !System.Boolean.TryParse( args[ ++i ].TrimToNull(), out ignoreCase ) ) {
-							PrintUsage();
-							return 1;
-						}
-					} else {
-						PrintUsage();
-						return 1;
-					}
-				} while ( i < len );
+
+			var processor = new Icod.Argh.Processor(
+				new Icod.Argh.Definition[] {
+					new Icod.Argh.Definition( "help", new System.String[] { "-h", "--help", "/help" } ),
+					new Icod.Argh.Definition( "copyright", new System.String[] { "-c", "--copyright", "/copyright" } ),
+					new Icod.Argh.Definition( "input", new System.String[] { "-i", "--input", "/input" } ),
+					new Icod.Argh.Definition( "output", new System.String[] { "-o", "--output", "/output" } ),
+					new Icod.Argh.Definition( "name", new System.String[] { "-n", "--name", "/name" } ),
+					new Icod.Argh.Definition( "ignoreCase", new System.String[] { "-ic", "--ignoreCase", "/ignoreCase" } ),
+				},
+				System.StringComparer.OrdinalIgnoreCase
+			);
+			processor.Parse( args );
+			if ( processor.Contains( "help" ) ) {
+				PrintUsage();
+				return 1;
+			} else if ( processor.Contains( "copyright" ) ) {
+				PrintCopyright();
+				return 1;
 			}
 
 			System.Func<System.String?, System.Collections.Generic.IEnumerable<System.String>> reader;
-			if ( System.String.IsNullOrEmpty( inputPathName ) ) {
-				reader = a => ReadStdIn();
+			if ( processor.TryGetValue( "input", true, out var inputPathName ) ) {
+				if ( System.String.IsNullOrEmpty( inputPathName ) ) {
+					PrintUsage();
+					return 1;
+				} else {
+					reader = a => ReadFile( a! );
+				}
 			} else {
-				reader = a => ReadFile( a! );
+				reader = a => ReadStdIn();
 			}
 
 			System.Action<System.String?, System.Collections.Generic.IEnumerable<System.String>> writer;
-			if ( System.String.IsNullOrEmpty( outputPathName ) ) {
-				writer = ( a, b ) => WriteStdOut( b );
+			if ( processor.TryGetValue( "input", true, out var outputPathName ) ) {
+				if ( System.String.IsNullOrEmpty( outputPathName ) ) {
+					PrintUsage();
+					return 1;
+				} else {
+					writer = ( a, b ) => WriteFile( a!, b );
+				}
 			} else {
-				writer = ( a, b ) => WriteFile( a!, b );
+				writer = ( a, b ) => WriteStdOut( b );
+			}
+
+			if ( processor.TryGetValue( "name", true, out var name ) ) {
+				if ( System.String.IsNullOrEmpty( name ) ) {
+					PrintUsage();
+					return 1;
+				}
+			} else {
+				name = "EN-US";
+			}
+
+			if ( !processor.TryGetValue( "ignoreCase", true, out var ignoreCaseStr ) ) {
+				ignoreCaseStr = "true";
+			}
+			if ( !System.Boolean.TryParse( ignoreCaseStr, out var ignoreCase ) ) {
+				PrintUsage();
+				return 1;
 			}
 
 			var read = reader( inputPathName ).ToList().Distinct( GetComparer( name, ignoreCase ) );
@@ -97,7 +108,7 @@ namespace Icod.Distinct {
 			System.Console.Error.WriteLine( "If inputFilePathName is omitted then input is read from StdIn." );
 			System.Console.Error.WriteLine( "If outputFilePathName is omitted then output is written to StdOut." );
 			System.Console.Error.WriteLine( "The name switch specifies the name of a culture, which is not case-sensitive." );
-			System.Console.Error.WriteLine( "The ignoreCase switch should be true to specify that comparison operations be case-insensitive; false to specify that comparison operations be case-sensitive." );
+			System.Console.Error.WriteLine( "The ignoreCase switch should be true to specify that comparison operations be case-insensitive; false to specify that comparison operations be case-sensitive.  The default is true." );
 		}
 		private static void PrintCopyright() {
 			var copy = new System.String[] {
@@ -137,11 +148,7 @@ namespace Icod.Distinct {
 				line = System.Console.In.ReadLine();
 			}
 		}
-		private static System.Collections.Generic.IEnumerable<System.String> ReadFile( System.String? filePathName ) {
-			filePathName = filePathName?.TrimToNull();
-			if ( System.String.IsNullOrEmpty( filePathName ) ) {
-				throw new System.ArgumentNullException( nameof( filePathName ) );
-			}
+		private static System.Collections.Generic.IEnumerable<System.String> ReadFile( System.String filePathName ) {
 			using ( var file = System.IO.File.Open( filePathName, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read ) ) {
 				using ( var reader = new System.IO.StreamReader( file, System.Text.Encoding.UTF8, true, theBufferSize, true ) ) {
 					var line = reader.ReadLine();
@@ -161,11 +168,7 @@ namespace Icod.Distinct {
 				System.Console.Out.WriteLine( datum );
 			}
 		}
-		private static void WriteFile( System.String? filePathName, System.Collections.Generic.IEnumerable<System.String> data ) {
-			filePathName = filePathName?.TrimToNull();
-			if ( System.String.IsNullOrEmpty( filePathName ) ) {
-				throw new System.ArgumentNullException( nameof( filePathName ) );
-			}
+		private static void WriteFile( System.String filePathName, System.Collections.Generic.IEnumerable<System.String> data ) {
 			using ( var file = System.IO.File.Open( filePathName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write, System.IO.FileShare.None ) ) {
 				_ = file.Seek( 0, System.IO.SeekOrigin.Begin );
 				using ( var writer = new System.IO.StreamWriter( file, System.Text.Encoding.UTF8, theBufferSize, true ) ) {
