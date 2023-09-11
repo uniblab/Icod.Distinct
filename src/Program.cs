8dc79f934dd57b1b38e925a78651b159ad5185e7
uniblab <uniblab@hotmail.com>
2023-09-11 00:06:41 -0400

    typo

--- src/Program.cs
+++ src/Program.cs
@@ -63,7 +63,7 @@ namespace Icod.Distinct {
 			}
 
 			System.Action<System.String?, System.Collections.Generic.IEnumerable<System.String>> writer;
-			if ( processor.TryGetValue( "input", true, out var outputPathName ) ) {
+			if ( processor.TryGetValue( "output", true, out var outputPathName ) ) {
 				if ( System.String.IsNullOrEmpty( outputPathName ) ) {
 					PrintUsage();
 					return 1;
